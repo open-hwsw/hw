83fc90eb4c87682cc53cea96c846375745625c72
zxxswag <1471546187@qq.com>
2024-12-24 23:33:40 +0800

    up

--- dv/script/python/makefile/makefile_common.py
+++ dv/script/python/makefile/makefile_common.py
@@ -1,3 +1,31 @@
 CPUS = $(shell nproc)
 MODE = $(shell getconf LONG_BIT)
+SEED = $(shell data +%m%d%H%M%S)
 SVN_REV = $(shell svn info | grep 'Revision' | awk '{print $$2}')
+
+ifeq ($(MODE), 64)
+    VCS_CMP_OPTS += -full64
+endif
+
+UVM_EN ?= 1
+UVM_TC ?=
+
+ifdef UVM_TC
+    TC_FULL_NAME = $(UVM_TC)_$(SEED)
+endif
+
+ifeq ($(UVM_EN), 1)
+    VCS_CMP_OPTS += +define+UVM +UVM_TESTNAME=$(UVM_TC)
+endif
+
+SV_EN ?= 1
+ifeq ($(SV_EN), 1)
+    VCS_CMP_OPTS += -sverilog
+endif
+
+LOG_DIR ?= log
+CMP_LOG_DIR ?= $(LOG_DIR)/cmp
+SIM_LOG_DIR ?= $(LOG_DIR)/sim
+
+VCS_CMP_OPTS += -l $(CMP_LOG_DIR)/cmp.log
+VCS_SIM_OPTS += -l $(SIM_LOG_DIR)/$(TC_FULL_NAME).log
