007c3186182e943f9d286c0d20e241b700b972a8
zxxswag <1471546187@qq.com>
2024-12-27 00:27:51 +0800

    up

--- dv/script/python/makefile/makefile_verdi.py
+++ dv/script/python/makefile/makefile_verdi.py
@@ -6,7 +6,7 @@ ifeq ($(filter $(VERDI_WORK_MODE), hardwareDebug interactiveDebug powerDebug ass
  	$(error "invalid verdi work mode: $(VERDI_WORK_MODE)")
 endif
 
-VERDI_OPTS += -workMode $(VERDI_WORK_MODE)
+VERDI_OPTS += -workMode $(VERDI_WORK_MODE) -rcFile novas.rc -guiConf novas.conf
 
 .PHONY: clean
 
